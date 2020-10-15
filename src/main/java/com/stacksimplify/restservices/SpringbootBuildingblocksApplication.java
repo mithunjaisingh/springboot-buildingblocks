1c23402ab45958cd2742faff9306f4d1bd70ff59
mithunnathan <mithunnathan@gmail.com>
2020-10-15 14:04:05 +0530

    Second Commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -3,6 +3,7 @@ package com.stacksimplify.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//SecondCommit
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
